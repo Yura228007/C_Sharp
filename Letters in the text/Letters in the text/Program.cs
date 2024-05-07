61b2c286b9f242f0ea9150307a453359a750c466
NovaS1de <nova29e@gmail.com>
2024-05-07 19:21:35 +0400

    Add Letters in the text

---
+++ Letters in the text/Letters in the text/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+
+        Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+        foreach (char letter in text.ToLower())
+        {
+            if (Char.IsLetter(letter))
+            {
+                if (letterCount.ContainsKey(letter))
+                {
+                    letterCount[letter]++;
+                }
+                else
+                {
+                    letterCount[letter] = 1;
+                }
+            }
+        }
+
+        foreach (KeyValuePair<char, int> pair in letterCount)
+        {
+            Console.WriteLine($"Буква: {pair.Key}, Количество: {pair.Value}");
+        }
+        Console.ReadKey();
+    }
+}
