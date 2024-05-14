f35b840587e1bb32f84fcc405be7d283550130b8
NovaS1de <nova29e@gmail.com>
2024-05-14 19:23:36 +0400

    Add CarProgram

---
+++ CarProgram/CarProgram/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog
+{
+    internal class Car
+    {
+        public delegate void aHandler(Car c);
+        public event aHandler? collision;
+
+        string Name { set; get; }
+
+        int Life { set; get; }
+        public Car(int life, string n)
+        {
+            Life = life;
+            Name = n;
+        }
+
+        public void update(int a)
+        {
+            if (a == 3) collision(this);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ CarProgram/CarProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using prog;
+
+namespace CarProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car(3, "light");
+            c.collision += (Car cur_car) => { Console.WriteLine(cur_car + "hitted!"); };
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("-");
+                c.update(random.Next(1, 5));
+            }
+        }
+    }
+}
