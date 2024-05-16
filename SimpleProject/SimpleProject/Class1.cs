a3a0d6275f1739056ceab12f58a91e1b1d6bd79d
NovaS1de <nova29e@gmail.com>
2024-05-16 19:21:55 +0400

    Add Student's_Marks

---
+++ SimpleProject/SimpleProject/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school
+{
+    internal class Student
+    {
+        public int mark;
+        public string Name { get; set; }
+        public bool went { get; set; }
+        public bool punished { get; set; }
+
+        public Student(string name, int mark1)
+        {
+            Name = name;
+            went = false;
+            punished = false;
+            mark = mark1;
+        }
+    }
+}
---
+++ SimpleProject/SimpleProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using school;
+namespace SimpleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            int[] arrayInt = { 5, 34, 67, 12, 94, 42 };
+                        IEnumerable<int> query = from i in arrayInt
+                                                 where i > 20 && i <50
+                                                 orderby i ascending
+                                                 select i;
+                        WriteLine("Only the even elements:");
+                        foreach (int item in query)
+                        {
+                            Write($"{item}\t");
+                        }
+                        WriteLine();
+                        System.Console.ReadKey();*/
+
+            List<Student> students = new List<Student> {
+                new Student("Mark", 3),
+                new Student("Bob", 4),
+                new Student("Tom", 2),
+                new Student("Ginger", 4),
+                new Student("Yura", 5),
+                new Student("Nikita", 2)
+            };
+
+            IEnumerable<Student> queryH = from student in students
+                                          where (student.mark > 4) && (student.went = false)
+                                          orderby student.mark descending
+                                          select student;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"day {i+1}");
+
+                if (queryH.Count() > 0)
+                {
+
+                }
+            }
+
+        }
+    }
+}
