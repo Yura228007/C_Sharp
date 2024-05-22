3097bdd9e17fe0b5906b8949e59fc22f78d2a251
Yura228007 <iuriishchedrin@gmail.com>
2024-05-23 03:08:42 +0400

    Add DirectoriesFromFile

---
+++ DirectoriesForWorkers/DirectoriesForWorkers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesForWorkers
+{    
+    internal class Program
+    {
+        static string path = "Structure";
+        static string pathOriginal = "Structure";
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if (dir.Exists)
+            {
+                dir.Delete(true);
+                dir.Create();
+            }
+            else
+            {
+                dir.Create(); // создаем каталог
+            }
+            using (FileStream fileStream = new FileStream("workers.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                    {
+                        string[] words = line.Split('\t');
+                        foreach (string word in words)
+                        {
+                            if (word != words[words.Length - 1])
+                            {
+                                path = Path.Combine(path, word);
+                                DirectoryInfo dir1 = new DirectoryInfo(path);
+                                if (!dir1.Exists) 
+                                {
+                                    dir1.Create(); 
+                                }
+                            }
+                            else
+                            {
+                                File.Create(path + $@"\{word}.txt");
+                            }
+                        }
+                        path = pathOriginal;
+                    }
+                }
+            }
+        }
+    }
+}
